a6011bec29eb208c9ba3895a6e6a100f4566b9e0
Matt Barlow <matt.barlow@rackspace.com>
2013-12-19 07:06:35 -0600

    Refactor for style

--- jukebox.py
+++ jukebox.py
@@ -1,6 +1,6 @@
-import time
+class JukeBox:
 
-class Jukebox:
+
     def __init__(self):
         song = Song()
         song.name = "Cashout"
@@ -10,24 +10,24 @@ class Jukebox:
         self.queue = Queue()
         self.supported_media = ['CD']
     
-    def listsongs(self):
-        songlist = []
+    def list_songs(self):
+        song_list = []
         for song in self.songs:
-            songlist.append(song.name)
+            song_list.append(song.name)
 
-        return songlist
+        return song_list
 
-    def addsong(self, name, length):
+    def add_song(self, name, length):
         song = Song()
         song.name = name
         song.length = length
         self.songs.append(song)
 
-    def nextsong(self):
+    def next_song(self):
         self.queue.isplaying = True
         try:
-            self.queue.queuelist[0].startsong()
-            self.queue.queuelist.pop(0)
+            self.queue.queue_list[0].startsong()
+            self.queue.queue_list.pop(0)
 
         except:
             print "Queue is empty"
@@ -35,9 +35,11 @@ class Jukebox:
         finally:
             self.queue.isplaying = False
 
-class Mp3(Jukebox):
+class Mp3(JukeBox):
+
+
     def __init__(self):
-        Jukebox.__init__(self)
+        JukeBox.__init__(self)
         self.add_support()
 
     def add_support(self):
@@ -45,20 +47,25 @@ class Mp3(Jukebox):
 
 
 class Song:
-    def startsong(self):
+
+
+    def start_song(self):
         length = self.length
         print "{} is playing for {} minutes and {} seconds".format(
-                self.name, length.split(':')[0], length.split(':')[1])
+                self.name, length.split(':')[0], length.split(':')[1]
+                )
 
-    def endsong(self):
+    def end_song(self):
         length = self.length
         print "{} has ended".format(self.name)
 
 class Queue:
+
+
     def __init__(self):
-        self.queuelist = []
+        self.queue_list = []
         self.isplaying = False
 
-    def queuesong(self, song):
-        self.queuelist.append(song)
+    def queue_song(self, song):
+        self.queue_list.append(song)
 
--- jukebox_test.py
+++ jukebox_test.py
@@ -1,54 +1,54 @@
 import unittest
 import jukebox
 
-class JukeboxTests(unittest.TestCase):
+class JukeBoxTests(unittest.TestCase):
 
     def test_listsongs_method_returns_results(self):
-        jb = jukebox.Jukebox()
-        songs = jb.listsongs()
+        jb = jukebox.JukeBox()
+        songs = jb.list_songs()
         self.assertIsInstance(songs, list)
     
     def test_listsongs_method_returns_results(self):
-        jb = jukebox.Jukebox()
-        songs = jb.listsongs()
+        jb = jukebox.JukeBox()
+        songs = jb.list_songs()
         self.assertIsInstance(songs, list)
 
     def test_add_song_to_jukebox(self):
-        jb = jukebox.Jukebox()
-        jb.addsong("Counting Stars", "3:22")
+        jb = jukebox.JukeBox()
+        jb.add_song("Counting Stars", "3:22")
         self.assertEqual(jb.songs[-1].name, "Counting Stars")
 
     def test_zero_queue_size(self):
         queue = jukebox.Queue()
-        self.assertEqual(len(queue.queuelist), 0)
+        self.assertEqual(len(queue.queue_list), 0)
 
     def test_add_songs_to_queue(self):
         queue = jukebox.Queue()
-        jb = jukebox.Jukebox()
-        jb.addsong("Counting Stars", "3:22")
-        queue.queuesong(jb.songs[-1])
-        queue.queuesong(jb.songs[-1])
-        self.assertEqual(len(queue.queuelist), 2)
+        jb = jukebox.JukeBox()
+        jb.add_song("Counting Stars", "3:22")
+        queue.queue_song(jb.songs[-1])
+        queue.queue_song(jb.songs[-1])
+        self.assertEqual(len(queue.queue_list), 2)
 
     def test_jukebox_has_a_queue(self):
-        jb = jukebox.Jukebox()
-        self.assertEqual(len(jb.queue.queuelist), 0)
+        jb = jukebox.JukeBox()
+        self.assertEqual(len(jb.queue.queue_list), 0)
 
 
     def test_queue_plays_next_song(self):
-        jb = jukebox.Jukebox()
-        jb.addsong("Counting Stars", "3:22")
-        jb.addsong("Cashout", "4:33")
-        jb.addsong("Burnout", "2:33")
-        jb.queue.queuesong(jb.songs[0])
-        jb.queue.queuesong(jb.songs[1])
-        jb.queue.queuesong(jb.songs[2])
-        jb.nextsong()
+        jb = jukebox.JukeBox()
+        jb.add_song("Counting Stars", "3:22")
+        jb.add_song("Cashout", "4:33")
+        jb.add_song("Burnout", "2:33")
+        jb.queue.queue_song(jb.songs[0])
+        jb.queue.queue_song(jb.songs[1])
+        jb.queue.queue_song(jb.songs[2])
+        jb.next_song()
         self.assertEqual(jb.songs[0].name, "Cashout")
 
     def test_try_next_song_with_empty_queue(self):
-        jb = jukebox.Jukebox()
-        jb.nextsong()
+        jb = jukebox.JukeBox()
+        jb.next_song()
         self.assertRaises(IndexError)
 
 
